5c15b2336903050427e65f6a8648ef93680e5ce7
Bogdan Bog <nedbog@gmail.com>
2018-04-04 21:18:02 +0300

    add course coponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './components/course/course.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
