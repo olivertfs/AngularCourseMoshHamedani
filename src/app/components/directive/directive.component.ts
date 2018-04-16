be0807f251b4210bcc052ca063aa90d0e96d62ec
nedbog <nedbog@gmail.com>
2018-04-16 20:32:22 +0300

    Directives - ngSwitchCase, ngFor, ngClass, ngStyle, safe traversal operator: "?"

--- src/app/components/directive/directive.component.ts
+++ src/app/components/directive/directive.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectiveComponent implements OnInit {
 courses = [1, 2, 3];
+viewMode = 'map';
+otherCourses = [
+  {id: 1, name: 'course1'},
+  {id: 2, name: 'course2'},
+  {id: 3, name: 'course3'}
+];
+anotherCourses = [];
+isSelected = true;
+canSave = true;
+task = {
+  title: 'Review applications',
+  assigneeLevel1: {
+    name: 'John Smith'
+  },
+  assigneeLevel2: null
+};
 
   constructor() { }
 
@@ -15,6 +31,35 @@ courses = [1, 2, 3];
 
   areCoursesAvailable(): boolean {
     return this.courses.length > 0;
+  }
+
+  onAdd() {
+    this.otherCourses.push({id: 4, name: 'course4'});
+  }
+
+  onRemove(course) {
+    const index = this.otherCourses.indexOf(course);
+    this.otherCourses.splice(index, 1);
+  }
+
+  onChange(course) {
+    course.name += ' UPDATE';
+  }
+
+  loadCourses() {
+    this.anotherCourses = [
+      {id: 1, name: 'course1'},
+      {id: 2, name: 'course2'},
+      {id: 3, name: 'course3'}
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
+
+  onClick() {
+    this.isSelected = !this.isSelected;
   }
 
 }
