4edf597532c5c623f35387099299e42221d4c548
bogdan <bogdan@pop-os.localdomain>
2018-06-09 01:03:45 +0300

    add completed zippy exercise

---
+++ src/app/components/zippy/zippy.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.css']
+})
+export class ZippyComponent {
+// tslint:disable-next-line:no-input-rename
+  @Input('title') title: string;
+  isExpanded = true;
+
+  constructor() { }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
