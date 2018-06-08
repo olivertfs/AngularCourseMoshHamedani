e7087dc317f919adf2806aa66400de49650953f0
bogdan <bogdan@pop-os.localdomain>
2018-06-09 00:38:38 +0300

    create custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { ReusableComponent } from './components/reusable/reusable.component';
 import { PanelComponent } from './components/panel/panel.component';
 import { LikeComponent } from './components/like/like.component';
 import { DirectiveComponent } from './components/directive/directive.component';
+import { InputFormatDirective } from './directives/input-format.directive';
+import { ZippyComponent } from './components/zippy/zippy.component';
 
 const appRoutes: Routes = [
   { path: '', component: CourseComponent },
@@ -26,7 +28,8 @@ const appRoutes: Routes = [
   { path: 'article', component: ArticleComponent },
   { path: 'titlecase', component: TitlecaseComponent },
   { path: 'reusable', component: ReusableComponent },
-  { path: 'directive', component: DirectiveComponent }  
+  { path: 'directive', component: DirectiveComponent },
+  { path: 'zippy', component: ZippyComponent }
 ];
 
 
@@ -43,7 +46,9 @@ const appRoutes: Routes = [
     ReusableComponent,
     PanelComponent,
     LikeComponent,
-    DirectiveComponent
+    DirectiveComponent,
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
